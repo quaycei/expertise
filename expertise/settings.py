4f5f1d8cbc430e806530aba847bcb91cf875311b
Quayce Thomas <quayce@timsle.co>
2017-02-14 09:59:36 -0500

    update staticfiles

--- expertise/settings.py
+++ expertise/settings.py
@@ -155,10 +155,19 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
+SITE_ID = 1
 
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 
 SITE_ID = 1
